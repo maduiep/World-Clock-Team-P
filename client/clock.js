11c65974a1f7105ea8234ca68a8da612f6e97e97
kinkidycodes <51934846+kinkidycodes@users.noreply.github.com>
2020-08-09 16:45:38 +0100

    feature: scirpts

--- client/clock.js
+++ client/clock.js
@@ -1,1 +1,25 @@
+
 console.log('WeJapa Team P');
+
+fetch("https://world-clock.p.rapidapi.com/json/est/now", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "world-clock.p.rapidapi.com",
+		"x-rapidapi-key": "b440e25bf5msh5ac33c2f8f4b475p181cabjsn769b423c581e"
+	}
+})
+.then(response => {
+    console.log(response);
+    return response.json()
+  })
+  .then(time => {
+    console.log(time.currentDateTime);
+    console.log(time.utcOffset);
+    let dateTime = time.currentDateTime;
+    
+    console.log(dateTime);
+    document.getElementById("time-display").innerHTML = time.currentDateTime;
+  })
+  .catch(err => {
+    console.log(err);
+  });
